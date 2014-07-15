61feadc0e097b08234fa1979c5227581e8c021c2
lc00 <leannecheung00@gmail.com>
2014-07-15 12:01:07 -0600

    starting to generate grid

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
+var xCoord = [];
+var yCoord = [];
+
+var gridDisplay = "<div class='container'> 
+					<div class='row'>
+						<div class='col-md-2' id='x'" + xCoord + "_y" + yCoord + "></div>"	
+						"<div class='col-md-2' id='x'" + xCoord + "_y" + yCoord + "></div>"	
+
+
+					"</div>" // end for class="row"
+				"</div>" //end for class="container"
+
+
+
+
+
 
 
 
 
 $(document).on('ready', function() {
-
 
 
 });
