a7fb84d0d7f7327e02ac678283ca8f5da5d9df34
lc00 <leannecheung00@gmail.com>
2014-07-15 12:39:23 -0600

    still working on adding the gridDisplay into the body dynamically

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-var xCoord = [];
-var yCoord = [];
+var xCoord = 0;
+var yCoord = 0;
 
-var gridDisplay = "<div class='container'> 
-					<div class='row'>
-						<div class='col-md-2' id='x'" + xCoord + "_y" + yCoord + "></div>"	
-						"<div class='col-md-2' id='x'" + xCoord + "_y" + yCoord + "></div>"	
+var gridDisplay = "<div class='container'>" +
+					 "<div class='row'>" +
+					 	"<div class='col-md-2' id='x" + xCoord + "_y" + yCoord + "\'" + ">" +
 
 
-					"</div>" // end for class="row"
+						"</div>"	+
+						"<div class='col-md-2' id='x'" + xCoord + "_y" + yCoord + ">" +
+							"<div class='up-arrow'>" + 
+								"<div class='icon-up-bold'>" +
+								"</div>" +
+							"</div>" + 
+						"</div>" +	
+
+
+
+
+
+
+					"</div>" + // end for class="row"
 				"</div>" //end for class="container"
 
 
@@ -17,8 +29,7 @@ var gridDisplay = "<div class='container'>
 
 
 
-
 $(document).on('ready', function() {
-
+$('body').append(gridDisplay);
 
 });
